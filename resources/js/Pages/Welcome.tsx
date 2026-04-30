be38902f891b8bd643a89f4c26883af00548568d
Webminty <git@webminty.com>
2026-04-30 15:35:01 -0700

    Fix github icon error

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -6,7 +6,6 @@ import {
     ChevronRight,
     Code2,
     ExternalLink,
-    Github,
     Layers,
     Layout,
     Mail,
@@ -147,6 +146,20 @@ const faqItems = [
     },
 ];
 
+function GithubIcon({ className }: { className?: string }) {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            aria-hidden="true"
+            className={className}
+        >
+            <path d="M12 .5C5.65.5.5 5.65.5 12c0 5.08 3.29 9.39 7.86 10.91.58.11.79-.25.79-.56 0-.28-.01-1.02-.02-2-3.2.69-3.87-1.54-3.87-1.54-.52-1.32-1.27-1.67-1.27-1.67-1.04-.71.08-.7.08-.7 1.15.08 1.76 1.18 1.76 1.18 1.02 1.75 2.69 1.24 3.34.95.1-.74.4-1.24.72-1.53-2.55-.29-5.24-1.28-5.24-5.7 0-1.26.45-2.29 1.18-3.1-.12-.29-.51-1.46.11-3.04 0 0 .97-.31 3.18 1.18a11.1 11.1 0 0 1 5.79 0c2.21-1.49 3.18-1.18 3.18-1.18.62 1.58.23 2.75.11 3.04.74.81 1.18 1.84 1.18 3.1 0 4.43-2.69 5.41-5.25 5.69.41.36.78 1.06.78 2.14 0 1.55-.01 2.8-.01 3.18 0 .31.21.68.8.56C20.21 21.39 23.5 17.08 23.5 12 23.5 5.65 18.35.5 12 .5Z" />
+        </svg>
+    );
+}
+
 export default function Welcome() {
     const [darkMode, setDarkMode] = useState(false);
     const [progressValue, setProgressValue] = useState(68);
@@ -187,7 +200,7 @@ export default function Welcome() {
                                             rel="noopener noreferrer"
                                             aria-label="View on GitHub"
                                         >
-                                            <Github className="h-4 w-4" />
+                                            <GithubIcon className="h-4 w-4" />
                                         </a>
                                     </Button>
                                 </TooltipTrigger>
@@ -259,7 +272,7 @@ export default function Welcome() {
                                         target="_blank"
                                         rel="noopener noreferrer"
                                     >
-                                        <Github className="mr-2 h-4 w-4" />
+                                        <GithubIcon className="mr-2 h-4 w-4" />
                                         View Source
                                     </a>
                                 </Button>
